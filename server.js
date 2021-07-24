d98b083eb4a77e53305032980aeb11d4b0ba58b2
Alexander Alford <alexalford14@yahoo.com>
2021-07-24 00:24:41 -0500

    Server routing improvements.

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 
 console.log("This is a test.")
 
+
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 
 const app = express();
 
 app.set('view engine', 'ejs')
-
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static('./public'));
 
 
-//Server setup.
-let port = process.env.PORT;
-
-if(port == null || port == ""){
-    port = 3000;
-}
-
-// app.route("/")
-
-
-app.listen(port, () => {
-    console.log("Server is running on port " + port);
-});
 
 
 //Beginning of database setup.
-var mongoDB = "mongodb+srv://prototypecluster.rbkx4.mongodb.net/products";
+var mongoDB = "mongodb+srv://guest:temp@prototypecluster.rbkx4.mongodb.net/products";
 const productCollection = 'collection';
 
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -44,7 +28,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('disconnected', () => {
     console.log("MongoDB database disconnected successfully.")
 });
-
 db.once("open", () => {
     console.log("MongoDB database connection established.");
 })
@@ -64,10 +47,34 @@ const productModel = mongoose.model('Product', productSchema, productCollection)
 
 productModel.find((err, products) => {
     if (err) return console.error(err);
-    console.log(products);
+    // console.log(products);
 })
 //End of database setup.
 
 
+//Beginning of server setup.
+let port = process.env.PORT;
 
-ReactDOM.render( <h1> Hello!</h1>, document.getElementById("productDisplay") );
+if(port == null || port == ""){
+    port = 3000;
+}
+
+
+app.route('/')
+
+
+.get(function(req, res){
+
+      res.send('index.html'); 
+})
+
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
+//Emd of server setup.
+
+
+
+
+
