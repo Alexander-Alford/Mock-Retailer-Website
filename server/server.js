f7fdd3036b42e4626cd002bc1b463e5dbe373c21
Alexander-Alford <AlexAlford14@yahoo.com>
2021-08-22 14:37:14 -0500

    Hot fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('./public'));
-
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
 
